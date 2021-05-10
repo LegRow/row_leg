d059aa2656dc783a1c5dc841c69a47d2c0253b4c
Noel Hsiao <huihsinhsiao12@gmail.com>
2021-05-11 00:03:43 +0800

    user can edit and permitted parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,7 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :age, :tel, :email, :bank_account, :password])
+
+    devise_parameter_sanitizer.permit(:account_update, keys: [:tel, :email, :bank_account, :password])
   end
 end
