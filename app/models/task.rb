7d60415199032fb4fb8d14cd57e3a9f31b75266e
nauosika <nauosikaforwork@gmail.com>
2021-05-10 22:47:43 +0800

    驗證器wip event翻譯

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,7 @@ class Task < ApplicationRecord
   validates :brief_description, :description, :address_city, :address_district, :address_street, :store_name, :reward, presence: true
   
   
+  private
   
   def address
     [address_city, address_district, address_street].join
