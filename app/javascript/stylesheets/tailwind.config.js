c7e36f243123aec3b26a39f61207af4566628f60
yuricky <leoleo5994@gmail.com>
2021-05-11 01:45:54 +0800

    設置tailwind.config purge路徑

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,6 +1,11 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: [],
+  purge: [
+    './app/**/*.html.erb',
+    './app/helpers/*.rb',
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
