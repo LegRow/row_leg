26f68108b690c4cd2583ddc80ae1e700013f5380
hsufenglin <>
2021-05-11 10:48:37 +0800

    add db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_10_075900) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "brief_description"
     t.text "description"
@@ -30,9 +33,6 @@ ActiveRecord::Schema.define(version: 2021_05_10_075900) do
     t.datetime "deleted_at"
     t.index ["deleted_at"], name: "index_tasks_on_deleted_at"
   end
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
