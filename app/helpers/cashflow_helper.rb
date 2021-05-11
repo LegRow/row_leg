16348d683d28bce625877ddbd918555516dc577d
YCChen <t5204713910@gmail.com>
2021-05-11 14:47:48 +0800

    add methods to calculate newebpay encoding

---
+++ app/helpers/cashflow_helper.rb
@@ -0,0 +1,55 @@
+module CashflowHelper
+
+  # encoding
+  def encrypt_aes_sha256_uppercase(transaction_information, key, iv)
+    result = encrypt_aes(transaction_information, key, iv)
+    result = 'HashKey=' + key + '&' + result + '&HashIV=' + iv  
+    result = Digest::SHA256.hexdigest(result)
+    result = result.upcase
+    return result
+  end
+
+  def encrypt_aes(transaction_information, key, iv)
+    result = http_build_query(transaction_information)
+    result = addpadding(result)
+    result = openssl_encrypt(result, key, iv)
+    result = bin2hex(result)
+    result = trim(result)
+    return result
+  end
+
+  def http_build_query(informations)
+    result = []
+    for information in informations
+      result_part = CGI.escape(information[0]) + '=' + CGI.escape(information[1])
+      result.append(result_part)
+    end
+    return result.join("&")
+  end
+
+  def addpadding(string, blocksize = 32)
+    len = string.length
+    pad = blocksize - (len % blocksize)
+    string = string + pad.chr() * pad
+    return string
+  end
+
+  def openssl_encrypt(string, key, iv, cipher_method = 'aes-256-cbc')
+    cipher = OpenSSL::Cipher.new(cipher_method)
+    cipher.encrypt
+    cipher.iv = iv
+    cipher.key = key
+    result = cipher.update(string)
+    return result
+  end
+
+  def bin2hex(bytes)
+    hex_string = bytes.unpack('H*')
+    return hex_string
+  end
+
+  def trim(data)
+    return data.first.strip
+  end
+
+end
