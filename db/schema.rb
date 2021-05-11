cce7b837ff052ef4c5906b0fd63489d2c0bba89a
Noelhsiao <huihsinhsiao12@gmail.com>
2021-05-11 14:15:43 +0800

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_05_11_033817) do
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
