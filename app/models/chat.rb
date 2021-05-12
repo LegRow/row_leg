2345115de6e25dcb52c9c02ea52cf71d0dea0f84
hsufenglin <>
2021-05-12 11:30:51 +0800

    add Room

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,13 +1,5 @@
 class Chat < ApplicationRecord
-  validates :username , presence: true
 
-  def self.generate
-    adjectives = ['Ancient','Creative','Dangerous','Effective','Flying','Gilded']
-    nouns = ['Highway','Intern','Jackhammer','Lion','Master']
-    number = rand.to_s[2..4]
-    username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
-    create(username: username)
-  end
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username , presence: true
+
+  def self.generate
+    adjectives = ['Ancient','Creative','Dangerous','Effective','Flying','Gilded']
+    nouns = ['Highway','Intern','Jackhammer','Lion','Master']
+    number = rand.to_s[2..4]
+    username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
+    create(username: username)
+  end
 end
   
