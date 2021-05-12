41775847eaf5876005334499ab258b49d45cb98f
hsufenglin <>
2021-05-12 13:41:01 +0800

    add username

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
     nouns = ['Highway','Intern','Jackhammer','Lion','Master']
     number = rand.to_s[2..4]
     username = "#{adjectives.sample}-#{nouns.sample}-#{number}"
-    create(username: username)
+    
   end
 end
   
