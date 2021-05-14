165812a0232b26c1c3be867e1af45276896f6fb7
huihsinhsiao <huihsinhsiao12@gmail.com>
2021-05-15 03:29:08 +0800

    設置google & github登入

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'cashflow/index'
   post 'cashflow/thankyou'
   root 'tests#index'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :tests, only: [:index]
 
   resources :tasks do
