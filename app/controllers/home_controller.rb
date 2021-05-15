4e5be7e1064091f01e202410dc43245e921e97b4
huihsinhsiao <huihsinhsiao12@gmail.com>
2021-05-16 00:37:25 +0800

    更新不需密碼驗證

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
-class TestsController < ApplicationController
+class HomeController < ApplicationController
   before_action :check_user_column, only: :index
   def index
     
   end
 
   private
-  # 使用者登錄後確認銀行帳戶欄位是否為空，是的話導到編輯頁面
+  # 使用者登錄後確認銀行帳戶欄位是否為空，是的話導到編輯頁面，第二次再進入首頁不會再跳轉
   def check_user_column
     return if current_user.blank?
     return if session[:visited_check].present?
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   get 'cashflow/index'
   post 'cashflow/thankyou'
-  root 'tests#index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
+  root 'home#index'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+
   resources :tests, only: [:index]
 
   resources :tasks do
