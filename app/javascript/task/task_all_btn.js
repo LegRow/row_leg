050a22667bcae335ad3b7a3354ef1bf89d4c5a20
yuricky <leoleo5994@gmail.com>
2021-05-18 03:05:19 +0800

    安裝FontAwesome 修改header跟logo

--- app/javascript/task/task_all_btn.js
+++ app/javascript/task/task_all_btn.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("turbolinks:load", () => {
   const taskOpenBtn = document.querySelectorAll("#inf-open");
 
   taskOpenBtn.forEach((el) =>
