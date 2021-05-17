5b6fd4096d13e55c06780720835d5d15ccaae613
YCChen <t5204713910@gmail.com>
2021-05-17 16:14:39 +0800

    delete duplicate routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,6 @@ Rails.application.routes.draw do
   resources :rooms, only: [:index, :show]
   resources :messages, only: [:index, :new, :create]
 
-  devise_for :users
-  resources :tests, only: [:index]
-
   resources :tasks do
     member do
       get 'confirm_applicant'
