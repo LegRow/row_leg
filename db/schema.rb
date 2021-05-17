8351938903ca65a65832f47085109f802761a0fd
hsufenglin <>
2021-05-17 10:30:12 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_184509) do
+ActiveRecord::Schema.define(version: 2021_05_16_070044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,11 @@ ActiveRecord::Schema.define(version: 2021_05_14_184509) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["task_id"], name: "index_orders_on_task_id"
+  end
+
+  create_table "qrcodes", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -60,6 +65,12 @@ ActiveRecord::Schema.define(version: 2021_05_14_184509) do
     t.string "state"
     t.index ["deleted_at"], name: "index_tasks_on_deleted_at"
     t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
