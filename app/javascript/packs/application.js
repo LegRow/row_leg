8783e48d1c1d79b10b617be747d5c9345185ae11
YCChen <t5204713910@gmail.com>
2021-05-18 14:03:39 +0800

    add js for google dynamic map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,10 @@
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "stylesheets/application"
+import "google_map"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-import "stylesheets/application"
