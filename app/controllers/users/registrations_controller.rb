ac907a333a93eda185d6661c9ec9a37817afdaef
huihsinhsiao <huihsinhsiao12@gmail.com>
2021-05-19 18:27:39 +0800

    清洗欄位的驗證移到users/registrations

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -44,15 +42,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.update_without_password(params)
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  # 清洗註冊欄位
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :age, :tel, :email, :bank_account, :password])
+  end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  # 清洗更新欄位
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:tel, :bank_account, :password])
+  end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
