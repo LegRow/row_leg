ada3eab4dff277a9b985ec50f708657e886e9ce0
YCChen <t5204713910@gmail.com>
2021-05-19 19:37:12 +0800

    successfully update position of employee every 30 seconds

--- app/javascript/google_map/index.js
+++ app/javascript/google_map/index.js
@@ -18,22 +18,48 @@ document.addEventListener('turbolinks:load', () => {
     // 以下是店家位置
     new google.maps.Map(maparea, option);
 
-    // 以下是 employee 的位置
-    const employeeLocationMap = new google.maps.Map(employeeMap, employeeMapOption);
-    const employeeLocation = [25, 121.5]
-    const markerLocations = [
-      [25.009571560497424, 121.46213302250602], // 店家位置
-      employeeLocation // employee 位置（這就是要依照我的位置改變）
-     ];
-    for(i  = 0;  i < markerLocations.length; i++) {
-      new google.maps.Marker({
-        position: new google.maps.LatLng(markerLocations[i][0], markerLocations[i][1]),
-        map: employeeLocationMap,
-      });
+    // trigger google map current position, once succeed then trigger succeed function
+    navigator.geolocation.getCurrentPosition(succeed, fail)
+
+    function succeed(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      employeeLocation = [latitude, longitude]
+
+      // 以下是 employee 的位置
+      let markerLocations = [
+        [25.009571560497424, 121.46213302250602], // 店家位置
+        employeeLocation // employee 位置（這就是要依照我的位置改變）
+       ];
+      
+      drawEmployeeAndStoreMarkers(markerLocations)
     }
+
+    function fail() {
+      console.log("error!")
+    }
+
+    function drawEmployeeAndStoreMarkers (markerLocations) {
+      const employeeLocationMap = new google.maps.Map(employeeMap, employeeMapOption);
+      for(let i  = 0;  i < markerLocations.length; i++) {
+        new google.maps.Marker({
+          position: new google.maps.LatLng(markerLocations[i][0], markerLocations[i][1]),
+          map: employeeLocationMap,
+        });
+        console.log(markerLocations)
+      }
+    }
+
+    console.log("testing")
+    // // 每隔 30 秒更新一次位置
+    // setInterval(function(e) {
+    //   if (employeeLocation) {
+    //     markerLocations.setPosition(loc) // This will move the marker to the new location
+    //   };
+    // }, 30000)
+
   }
 
-  // the function for google map script to callback
-  initMap()
+  // setInterval(initMap, 30000)
 
 })
