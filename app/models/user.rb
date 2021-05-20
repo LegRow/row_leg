a1e7949487e7f76ab567ed5bbb038b89d0f10590
huihsinhsiao <huihsinhsiao12@gmail.com>
2021-05-20 22:43:12 +0800

    User model 新增 未填寫的選項

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,10 @@ class User < ApplicationRecord
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email
-      user.name = provider_data.info.name
+      user.name = provider_data.info.name || "未填寫"
+      user.bank_account = "未填寫"
+      user.tel = "未填寫"
       user.password = Devise.friendly_token[0, 20]
     end
   end
 end
- 
