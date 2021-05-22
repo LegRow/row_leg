bbfcc8f91dbc6f08231813cff2d2f2ca9c0b2d35
huihsinhsiao <huihsinhsiao12@gmail.com>
2021-05-22 19:43:05 +0800

    fix Zeitwerk::NameError

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module GemYarnInit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,5 +19,6 @@ module GemYarnInit
     config.time_zone = "Taipei"
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = "zh-TW"
+    config.autoloader = :classic
   end
 end
