dc6a1e03a438c9f9e867fd86df7848040b62c1ea
yuricky <leoleo5994@gmail.com>
2021-05-24 04:31:51 +0800

    sign-up validate WIP

---
+++ app/javascript/view-event/form_validate.js
@@ -0,0 +1,46 @@
+document.addEventListener("turbolinks:load", () => {
+  const signupForm = document.querySelector("#signup-form");
+  if (signupForm) {
+    const email = document.querySelector(".email");
+    const name = document.querySelector(".name");
+    const age = document.querySelector(".age");
+    const tel = document.querySelector(".tel");
+    const bankAccount = document.querySelector(".bank-account");
+    const password = document.querySelector(".password");
+    const password2 = document.querySelector(".password2");
+    const signupBtn = document.querySelector(".btn");
+
+    const inputArr = [email, name, age, tel, bankAccount, password, password2];
+    // 檢查欄位是否填寫
+    const checkRequired = function (inputArr) {
+      inputArr.forEach((input) => {
+        if (input.value === "") {
+          input.parentElement.className = "field error";
+          input.nextElementSibling.textContent = `${input.parentElement.firstElementChild.textContent}不能為空`;
+        } else {
+          input.parentElement.className = "field success";
+        }
+      });
+    };
+
+    // 檢查長度
+    const checkLength = function (input, min) {
+      if (input.value.length < min) {
+        input.parentElement.className = "field error";
+        input.nextElementSibling.textContent = `${input.parentElement.firstElementChild.textContent}不能小於${min}碼`;
+      } else {
+        input.parentElement.className = "field success";
+      }
+    };
+
+    signupBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      checkRequired(inputArr);
+
+      checkLength(tel, 10);
+      checkLength(bankAccount, 10);
+      checkLength(password, 6);
+    });
+  }
+});
