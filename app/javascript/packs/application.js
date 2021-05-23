4b9db964bb0b0b677e750c0ec96c05515563322c
hsufenglin <>
2021-05-23 10:05:47 +0800

    routes modidied

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "stylesheets/application";
-import "google_map";
 import "controllers";
 
 import "@fortawesome/fontawesome-free/css/all";
