5f8adf6d6203471d24e87a329586dd6d7ad83443
nauosika <nauosikaforwork@gmail.com>
2021-05-28 01:41:13 +0800

    Recode

--- app/javascript/packs/partner.js
+++ app/javascript/packs/partner.js
@@ -1,16 +1,21 @@
+function initSlideShow() {
+  const slides = document.querySelectorAll('.slide')
+  const time = 3000
+  let index = 0;
+  const addActive = (index) => { slides[index].classList.add('active') }
+  const removeActive = (index) => { slides[index].classList.remove('active') }
+
+  addActive(index)
+
+  setInterval( () => {
+    removeActive(index)
+    index++;
+    if (index === slides.length) index = 0;
+    addActive(index);
+  }, time);
+}
+
 document.addEventListener('turbolinks:load', function(){
   const slideshows = document.querySelectorAll('[data-component="slideshow"]');
   slideshows.forEach(initSlideShow);
-  function initSlideShow(slideshow) {
-  const slides = document.querySelectorAll('.slide');
-  let index = 0, time = 3000;
-	slides[index].classList.add('active');
-
-	setInterval( () => {
-		slides[index].classList.remove('active');
-    index++;
-		if (index === slides.length) index = 0;
-    slides[index].classList.add('active');
-    }, time);
-  }
 })
