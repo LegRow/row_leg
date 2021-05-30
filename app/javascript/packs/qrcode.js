fe9e93c9e8cb1f52b83de1a6c679f59a3aa8c6fd
hsufenglin <80436272+hsufenglin@users.noreply.github.com>
2021-05-30 21:46:42 +0800

    #251 (#259)
    * add qrcode packs
    
    * 聊天室分隔
    
    * qrcode improve
    
    Co-authored-by: YCChen <t5204713910@gmail.com>
    Co-authored-by: hsufenglin <>

--- app/javascript/packs/qrcode.js
+++ app/javascript/packs/qrcode.js
@@ -5,13 +5,15 @@ document.addEventListener('turbolinks:load', function(){
     const $qrcode = document.getElementById('qrcode');
     const message = $qrcode.dataset.message;
 
-    new QRCode($qrcode, {
-      text: message,
-      width: 128,
-      height: 128,
-      colorDark: "#000",
-      colorLight: "#fff",
-      correctLevel: QRCode.CorrectLevel.H,
-    });
-  }
+  const $qrcode = document.getElementById('qrcode');
+  const message = $qrcode.dataset.message;
+
+  new QRCode($qrcode, {
+    text: message,
+    width: 200,
+    height: 200,
+    colorDark: '#805300',
+    colorLight: '#fff',
+    correctLevel: QRCode.CorrectLevel.H
+  });
 })
