76f52558eb77418d34f4f05fd0c8982fa8ac979d
hsufenglin <unknown>
2021-05-31 15:37:50 +0800

    change const

--- app/javascript/packs/qrcode.js
+++ app/javascript/packs/qrcode.js
@@ -2,10 +2,10 @@ import QRCode from "davidshimjs-qrcodejs";
 
 document.addEventListener("turbolinks:load", function () {
   if (document.getElementById("qrcode")) {
-    const Qrcode = document.getElementById("qrcode");
-    const message = Qrcode.dataset.message;
+    const qrcode = document.getElementById("qrcode");
+    const message = qrcode.dataset.message;
 
-    new QRCode(Qrcode, {
+    new QRCode(qrcode, {
       text: message,
       width: 200,
       height: 200,
