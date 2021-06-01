18464b6499a5d2695afafabb3c93e32d3c7ede79
YCChen <t5204713910@gmail.com>
2021-06-01 13:10:41 +0800

    successfully post and redirect to newebpay

--- spec/requests/cashflow_spec.rb
+++ spec/requests/cashflow_spec.rb
@@ -1,48 +1,42 @@
 require 'rails_helper'
+include Newebpay
 
-RSpec.describe "newebpay" do
+RSpec.describe "newebpay", type: :feature do
 
-  include Capybara::DSL
+  # 這邊的測試比較像是 feature test，會以 selenium 去進行自動化操控，反正只要可以得到我們想得到的藍新回 post 即可
+  driver = Selenium::WebDriver.for :chrome
 
-  subject(:call) { post endpoint, params: params }
-  let(:endpoint) { '/cashflow/from_newebpay' }
-  let(:params) do # 這之後應該要由藍新打過來，ngrok
+  let(:website) do "14aa069f459c.ngrok.io" end # 這個之後應該要可以直接去抓 ngrok terminal 裡的網址，而且這個之後要不開啟網頁（headless），這是要接藍新的 post
+  let(:test_data) do
     {
-    "Status"=>"SUCCESS",
-    "MerchantID"=>"MS119996394",
-    "Version"=>"1.5",
-    "TradeInfo"=>"62cff73d959a9296af4e1cb51de6514fa569f7875a2d52abf0afd53d725f7f9810d71ab542981e12b708d2e1ea25d1cb0768078886297c0dd4cc3483d9c11bb58ab3015d557a56bfd26a5946d017ae3c092e701ca7436c00e287ebfd03d3c35cc9517360f0d21c622e71749498a15318f05a53e9dde14dbf60815015018177357acc3a5397ba23fdae26cce6b90f0276670a5ebfb9711aaa69ad541ec01784d2b26812ed25e09d1f4e1b0681c36fcd06332e3ba593501c70cb8c2fb207e0c3237ca10ba648db51989430fedd9263bb7cab59771855904f0f27ccc040ae45157fe2a58b0a2652c17b006de3559e2745de59a8d2b0e93fdf7f0c22283496c3d048beb2f83764a285b4cba2876aa1b37ac8fbddb3ccbd6753eb37d0dd414f99c0e9251b5790574659465ccb4fd45143d0de740969af9fa7059769c20ba43bdea70b",
-    "TradeSha"=>"365BDFD74B7B23BA012E77165A57361634742B15DBDD3F4666DC01290C4C0991"
+      reward: '200', # 可以是隨便的金額
+      behalf: '90', # 可以是隨便的金額
+      order_number: 'testing123' # 可以是隨便 order number
     }
   end
 
-  # context "when sending to newebpay" do
-  #   it "successfully encodes" do
+  context "when employer create task" do
+    it "successfully pay" do
+      # http://localhost:3000/cashflow/to_newebpay?for_newebpay%5Bbehalf%5D=90&for_newebpay%5Border_number%5D=1622520937&for_newebpay%5Breward%5D=200
+      url_to_newebpay = URI::HTTP.build(
+        :host => website,
+        :path => '/cashflow/to_newebpay',
+        :query =>
+          {
+            reward: test_data["reward"],
+            behalf: test_data["behalf"],
+            order_number: test_data["merchant_order_number"]
+          }.to_query
+      )
+      puts "==============="
+      puts url_to_newebpay
+      driver.navigate.to url_to_newebpay
+      sleep 10.second
 
-  #   end
-  # end
+      # assertion: get the post of success & the correct decoded 'MerchantOrderNo'
 
-  # # posts from newebpay
-  # context "when newebpay posts" do
-  #   it "returns success response" do
-  #     call
-  #     expect(response).to be_successful
-  #   end
-  # end
-
-  # # get to newebpay
-  # context "when we pay" do
-  #   it ""
-  # end
-
-  # # all other methods to use posts from newebpay and get to newebpay
-  # context "when employer create task" do
-  #   it "successfully pay" do
-  #     # arrange: go to "./tasks/new" -> input fake data -> click "新增"
-  #     # actions: redirect to "newebpay"
-  #     # assertion: get the post of success
-  #   end
-  # end
+    end
+  end
 
   # context "when employer click pay" do
   #   it "successfully pay" do
