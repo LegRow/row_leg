a21d1fd4eff588d9fbf2dffca12c19d6a74bdf7a
nauosika <nauosikaforwork@gmail.com>
2021-06-01 21:22:47 +0800

    a對方說話有大頭貼

--- app/jobs/send_message_job.rb
+++ app/jobs/send_message_job.rb
@@ -4,11 +4,11 @@ class SendMessageJob < ApplicationJob
   def perform(message, current_user)
     mine = ApplicationController.render(
       partial: 'messages/message',
-      locals: {message: message, current_user: current_user}
+      locals: {message: message, current_user: current_user},
       )
     theirs = ApplicationController.render(
       partial: 'messages/message',
-      locals: {message: message, current_user: nil}
+      locals: {message: message, current_user: nil},
       )
 
     ActionCable.server.broadcast "room_channel_#{message.room_id}", mine: mine, theirs: theirs, message: message, type: 'message'
