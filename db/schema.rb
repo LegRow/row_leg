09d7bfcd5125ec675fdee2e7dcad68ee9a3db8f6
yuricky <leoleo5994@gmail.com>
2021-06-04 00:39:07 +0800

    修正無法執行foreman的bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_31_133940) do
+ActiveRecord::Schema.define(version: 2021_06_02_145941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,14 +108,11 @@ ActiveRecord::Schema.define(version: 2021_05_31_133940) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "gender"
     t.string "tel"
-    t.integer "age"
     t.string "bank_account"
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 500, default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["gender"], name: "index_users_on_gender"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
