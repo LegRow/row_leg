2729e5914962740751fcbbe6cec725a8e0ac5071
YCChen <t5204713910@gmail.com>
2021-06-03 13:00:46 +0800

    factory for fake data for newebpay test

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,11 +6,15 @@ FactoryBot.define do
     address_district { "MyString" }
     address_street { "MyString" }
     store_name { "MyString" }
-    reward { 1 }
-    behalf { 1 }
-    task_at { "2021-05-10 14:01:11" }
-    task_end { "2021-05-10 14:01:11" }
+    reward { 2000 }
+    behalf { 90 }
+    task_at { 4.hours.from_now }
+    task_end { 6.hours.from_now }
     remarks { "MyString" }
     status { "MyString" }
+    association :user, email: Faker::Internet.email, password: 'testtest'
+    after(:create) do |task|
+      task.order ||= create(:order, :task => task)
+    end
   end
 end
